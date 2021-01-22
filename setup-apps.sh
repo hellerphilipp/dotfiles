4a282a15da748f3d5fc02a22935d7e72ff8c0e47
Philipp Heller <10487949+hellerphilipp@users.noreply.github.com>
2021-01-22 15:13:00 +0100

    Automate iTerm2 configuration

--- setup-apps.sh
+++ setup-apps.sh
@@ -5,6 +5,12 @@
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2"
+
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
 ###############################################################################
 # Google Chrome                                                               #
 ###############################################################################
